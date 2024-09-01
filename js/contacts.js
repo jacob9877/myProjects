56d7695268dc7b9725a8209e297c0e384ab7a38c
Mark Bruckert <markbruckert1@Gmail.com>
2024-09-01 15:30:08 -0400

    Deleting Contacts

--- js/contacts.js
+++ js/contacts.js
@@ -4,7 +4,7 @@ function getContacts() {
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ userId: 1 }),
+    body: JSON.stringify({ userId: 2 }),
   })
     .then((response) => {
       return response.json();
@@ -26,7 +26,7 @@ function displayContacts(contacts) {
           <h3>${contact.name}</h3>
           <p>${contact.email}</p>
         </div>
-        <button class="contact-item-button">
+        <button class="contact-item-button" onclick="deleteContact('${contact.id}')">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="contact-item-button-icon">
             <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
           </svg>
@@ -53,7 +53,7 @@ function displayContactDetails(contact) {
           <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
         </svg>      
         </button>
-        <button class="contact-item-button">
+        <button class="contact-item-button" onclick="deleteContact('${contact.id}')">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="contact-item-button-icon">
             <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
           </svg>
@@ -75,4 +75,32 @@ function displayContactDetails(contact) {
       </div>
     </div>
   `;
+}
+
+function displayContactDetailsEmpty() {
+  const contactDetails = document.querySelector(".contact-details");
+  contactDetails.innerHTML = `
+<div class="contact-image-container">
+  <img src="empty.png" class="contact-image" />
+  <h2>Select a Contact from the Sidebar</h2>
+</div>`;
+}
+
+function deleteContact(contactId) {
+  fetch(
+    `http://cop4331-project.online/LAMPAPI/DeleteContact.php?id=${contactId}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      displayContactDetailsEmpty();
+      getContacts();
+    });
 }
